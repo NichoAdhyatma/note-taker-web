263baabbb4c52a5ae09ed65cc0ae54d208305555
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-05-06 08:40:25 +0700

    sticky-foter

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import AppLogo from "../assets/app-logo.png";
 
 export default function Footer() {
   return (
-    <footer className="text-md font-semibold flex gap-4 justify-center items-center p-4 bg-white drop-shadow-md">
+    <footer className="text-md font-semibold flex gap-4 justify-center items-center p-4 bg-white drop-shadow-md sticky bottom-0 z-10">
       <Image src={AppLogo} alt={"app-logo"} width={30} height={30} />© 2023 Note
       Taker.
     </footer>
